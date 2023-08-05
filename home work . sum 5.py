3d99e95d3e2212b30a739041083a907cf2829bc1
jigar-python <140880779+jigar-python@users.noreply.github.com>
2023-08-05 13:21:57 +0530

    Add files via upload

---
+++ home work . sum 5.py
@@ -0,0 +1,10 @@
+no1=int(input(" enter the no : " ))
+
+no2=int(input(" enter the no : " ))
+
+if no1+no2==5 or no1-no2==5:
+     print("true")
+
+else :
+    print("false")
+    
---
+++ home work.even or odd.py
@@ -0,0 +1,8 @@
+a=int(input("enter the no :"))
+
+if (a%2)==0 :
+    print("the no is even")
+
+else :
+    print("the no is odd")
+    
